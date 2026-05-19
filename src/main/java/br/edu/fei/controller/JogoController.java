aa9a9cfba2c26a7cb141e27ed5a46af17436ac2a
Kamiladl-2804 <kamila.luongo@aluno.viscondeitaborai.com.br>
2026-05-18 22:02:04 -0300

    teste

---
+++ src/main/java/br/edu/fei/controller/JogoController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.fei.
+
+import br.edu.fei.model.Frase;
+import br.edu.fei.model.GerenciadorFrases;
+import br.edu.fei.view.TelaDigitacao;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author feispkluongo
+ */
+
+
+public class JogoController {
+
+    
+}
+
